95393c5900915c1c3fb938413e642281c6c41989
Wahaj <100323687+wahajals@users.noreply.github.com>
2022-08-13 19:59:51 +0300

    added project

--- cw5/cw5/ContentView.swift
+++ cw5/cw5/ContentView.swift
@@ -10,50 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @State var counter: [Int] = [0, 0, 0]
     var body: some View {
+        ZStack {
+            Image("Praying")
+                .resizable()
+                .ignoresSafeArea()
+                .blur(radius:6)
+            Color.black
+                .ignoresSafeArea()
+                .opacity(0.4)
         VStack{
-            HStack{
-                Text("أستغفر الله العظيم").font(.title)
-                Spacer()
-                Text("\(counter[0])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[0] = counter[0] + 1
-                    }
-            }.padding()
-            HStack{
-                Spacer()
-                Text("الحمدلله").font(.title)
-                Spacer()
-                Text("\(counter[1])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[1] = counter[1] + 1
-                    }
-            }.padding()
-            HStack{
-                Text("سبحان الله وبحمده").font(.title)
-                Spacer()
-                Text("\(counter[2])")
-                    .font(.largeTitle)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .foregroundColor(.white)
-                    .background(Color.green)
-                    .clipShape(Circle())
-                    .padding()
-                    .onTapGesture {
-                        counter[2] = counter[2] + 1
-                    }
-            }.padding()
+            Text("المسبحة")
+                .font(.largeTitle)
+                .bold()
+                .foregroundColor(.primary)
+                .padding(.vertical)
+                .background(.secondary)
+            
+            AthkarView(myCounter: $counter[0], title: "استغفرالله")
+            AthkarView(myCounter: $counter[1],title: "الحمدلله")
+            AthkarView(myCounter: $counter[2],title:"سبحان الله وبحمده")
+     
+        }
         }
     }
 }
@@ -64,5 +41,29 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
+    }
+}
+
+struct AthkarView: View {
+    @Binding var myCounter: Int
+    @State var title: String
+    var body: some View {
+        HStack{
+            Text(title).font(.title)
+                .foregroundColor(.white)
+                .bold()
+            Spacer()
+            Text("\(myCounter)")
+                .font(.largeTitle)
+                .frame(width: 100, height: 100, alignment: .center)
+                .foregroundColor(.white)
+                .background(Color.secondary)
+                .clipShape(Circle())
+                .padding()
+                .onTapGesture {
+                    myCounter = myCounter + 1
+                }
+        }.padding()
     }
 }
